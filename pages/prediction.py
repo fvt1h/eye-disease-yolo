2917fb9c4ba42a2d74ff37345a1cfc6ad7e4b248
fvt1h <FatihMuhammad724@gmail.com>
2024-11-18 19:10:22 +0700

    perubahan pada predict

--- pages/prediction.py
+++ pages/prediction.py
@@ -17,7 +17,6 @@ def load_model(model_option):
     available_models = {
         "YOLOv11n": "./models/best_yoloV11n.pt",
         "YOLOv11s": "./models/best_yoloV11s.pt",
-        #"YOLOv11m": "./models/best_yoloV11m.pt",
     }
     model_path = available_models.get(model_option)
     if model_path:
@@ -33,40 +32,41 @@ def predict(image, model):
     results.render()
     return results.imgs[0], results.pandas().xyxy[0]
 
-# Tampilan Streamlit
-st.title("Halaman Prediksi")
+# Fungsi utama untuk halaman prediksi
+def show():
+    st.title("Halaman Prediksi")
 
-model_option = st.selectbox("Pilih Model", ["YOLOv11n", "YOLOv11s"]) #"YOLOv11m"
-uploaded_file = st.file_uploader("Unggah Gambar", type=["jpg", "png", "jpeg"])
+    model_option = st.selectbox("Pilih Model", ["YOLOv11n", "YOLOv11s"])
+    uploaded_file = st.file_uploader("Unggah Gambar", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None:
-    st.image(uploaded_file, caption="Gambar yang diunggah", use_container_width=True)
-    
-    image = create_opencv_image_from_stringio(uploaded_file)
-    model = load_model(model_option)
-    
-    if model:
-        pred_image, pred_data = predict(image, model)
-        st.image(pred_image, caption="Hasil Prediksi", use_column_width=True)
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Gambar yang diunggah", use_container_width=True)
         
-        st.write("Detail Prediksi:")
-        st.write(pred_data)
+        image = create_opencv_image_from_stringio(uploaded_file)
+        model = load_model(model_option)
         
-        # Simpan hasil ke database
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        conn = sqlite3.connect('history/prediction_history.db')
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS history
-                          (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                           file_name TEXT, 
-                           prediction TEXT, 
-                           model TEXT, 
-                           accuracy REAL, 
-                           timestamp TEXT)''')
-        
-        pred_data_str = str(pred_data)
-        cursor.execute("INSERT INTO history (file_name, prediction, model, accuracy, timestamp) VALUES (?, ?, ?, ?, ?)",
-                       (uploaded_file.name, pred_data_str, model_option, None, now))
-        conn.commit()
-        conn.close()
-        st.success("Hasil prediksi telah disimpan.")
+        if model:
+            pred_image, pred_data = predict(image, model)
+            st.image(pred_image, caption="Hasil Prediksi", use_column_width=True)
+            
+            st.write("Detail Prediksi:")
+            st.write(pred_data)
+            
+            # Simpan hasil ke database
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            conn = sqlite3.connect('history/prediction_history.db')
+            cursor = conn.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS history
+                              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                               file_name TEXT, 
+                               prediction TEXT, 
+                               model TEXT, 
+                               accuracy REAL, 
+                               timestamp TEXT)''')
+            
+            pred_data_str = str(pred_data)
+            cursor.execute("INSERT INTO history (file_name, prediction, model, accuracy, timestamp) VALUES (?, ?, ?, ?, ?)",
+                           (uploaded_file.name, pred_data_str, model_option, None, now))
+            conn.commit()
+            conn.close()
+            st.success("Hasil prediksi telah disimpan.")
