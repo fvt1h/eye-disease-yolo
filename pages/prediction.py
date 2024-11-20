13f7fbb5757056b68a7dc16e9a75c005441607cf
fvt1h <FatihMuhammad724@gmail.com>
2024-11-20 23:47:55 +0700

    bismillah

--- pages/prediction.py
+++ pages/prediction.py
@@ -27,9 +27,9 @@ def show():
 
     # Pilihan model
     models = {
-        "YOLOv11n": ".models/best_yoloV11n.pt",
-        "YOLOv11s": ".models/best_yoloV11s.pt",
-        #"YOLOv11m": ".models/best_yoloV11m.pt"
+        "YOLOv11n": "./models/best_yoloV11n.pt",
+        "YOLOv11s": "./models/best_yoloV11s.pt",
+        #"YOLOv11m": "./models/best_yoloV11m.pt"
     }
     model_name = st.selectbox("Pilih Model", list(models.keys()))
     
